1c1af6a54f3b8e52f8a2708a008eebee99d78590
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-03-31 20:27:19 -0500

    Expands webpack configuration to support dev configs
     - Having issues with 'splitChunks'. Maybe something to do internally
        with webpack-dev-server and its websockets??

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,16 +3,17 @@ const modules = require('./modules');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const appPackageJson = require(paths.appPackageJson);
 
+// Source maps are resource heavy and can cause out of memory issue for large source files.
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
+
 module.exports = function(webpackEnv) {
   const isEnvDevelopment = webpackEnv === 'development';
   const isEnvProduction = webpackEnv == 'production';
-
-  // Source maps are resource heavy and can cause out of memory issue for large source files.
-  const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== 'false';
 
   // TODO support HTTPS (get changes from create-react-app@3.4.1)
 
@@ -43,54 +44,112 @@ module.exports = function(webpackEnv) {
         ? 'source-map'
         : false
       : isEnvDevelopment && 'cheap-module-source-map',
-    entry: [paths.appIndexJs], // transform to array, to support hot loading
+    // These are the "entry points" to our application.
+    // This means they will be the "root" imports that are included in JS bundle.
+    entry: [
+      // Include an alternative client for WebpackDevServer. A client's job is to
+      // connect to WebpackDevServer by a socket and get notified about changes.
+      // When you save a file, the client will either apply hot updates (in case
+      // of CSS changes), or refresh the page (in case of JS changes). When you
+      // make a syntax error, this client will display a syntax error overlay.
+      // Note: instead of the default WebpackDevServer client, we use a custom one
+      // to bring better experience for Create React App users. You can replace
+      // the line below with these two lines if you prefer the stock client:
+      isEnvDevelopment && require.resolve('webpack-dev-server/client') + '?/',
+      // Finally, this is your app's code:
+      paths.appIndexJs,
+      // We include the app code last so that if there is a runtime error during
+      // initialization, it doesn't blow up the WebpackDevServer client, and
+      // changing JS code would still trigger a refresh.
+    ].filter(Boolean),
     output: {
-      path: paths.appBuild,
-      pathinfo: false, // only true if dev
-      filename: 'static/js/[name].[contenthash:8].js',
+      // The build folder.
+      path: isEnvProduction ? paths.appBuild : undefined,
+      // Add /* filename */ comments to generated require()s in the output.
+      pathinfo: isEnvDevelopment,
+      // There will be one main bundle, and one file per asynchronous chunk.
+      // In development, it does not produce real files.
+      filename: isEnvProduction
+        ? 'static/js/[name].[contenthash:8].js'
+        : isEnvDevelopment && 'static/js/bundle.js',
+      // TODO: remove this when upgrading to webpack 5
       futureEmitAssets: true,
-      chunkFilename: 'static/js/[name].[contenthash:8].js',
-      devtoolModuleFilenameTemplate: info =>
-          path
-            .relative(paths.appSrc, info.absoluteResourcePath)
-            .replace(/\\/g, '/'),
-      jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+      // There are also additional JS chunk files if you use code splitting.
+      chunkFilename: isEnvProduction
+        ? 'static/js/[name].[contenthash:8].chunk.js'
+        : isEnvDevelopment && 'static/js/[name].chunk.js',
+      // Webpack uses `publicPath` to determine where the app is being served from.
+      // It requires a trailing slash, or the file assets will get an incorrect path.
+      // We inferred the "public path" (such as / or /my-project) from homepage.
+      // We use "/" in development.
+      publicPath: publicPath,
+      // Point sourcemap entries to original disk location (format as URL on Windows)
+      devtoolModuleFilenameTemplate: isEnvProduction
+        ? info =>
+            path
+              .relative(paths.appSrc, info.absoluteResourcePath)
+              .replace(/\\/g, '/')
+        : isEnvDevelopment &&
+          (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
+      // this defaults to 'window', but by setting it to 'this' then
+      // module chunks which are built will work in web workers as well.
       globalObject: 'this',
     },
     optimization: {
       minimize: isEnvProduction,
-      splitChunks: {
-        chunks: 'all',
-        name: false,
-      },
+      // Automatically split vendor and commons
+      // https://twitter.com/wSokra/status/969633336732905474
+      // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+      // splitChunks: {
+      //   chunks: 'all',
+      //   name: false, // throws error
+      // },
+      // Keep the runtime chunk separated to enable long term caching
+      // https://twitter.com/wSokra/status/969679223278505985
+      // https://github.com/facebook/create-react-app/issues/5358
       runtimeChunk: {
         name: entrypoint => `runtime-${entrypoint.name}`,
       },
     },
     resolve: {
-      modules: ['node_modules', paths.appNodeModules],
-      extensions: paths.moduleFileExtensions
-        .map(ext => `.${ext}`),
+      // This allows you to set a fallback for where Webpack should look for modules.
+      // We placed these paths second because we want `node_modules` to "win"
+      // if there are any conflicts. This matches Node resolution mechanism.
+      // https://github.com/facebook/create-react-app/issues/253
+      modules: ['node_modules', paths.appNodeModules].concat(
+        modules.additionalModulePaths || []
+      ),
+      // These are the reasonable defaults supported by the Node ecosystem.
+      extensions: paths.moduleFileExtensions.map(ext => `.${ext}`),
       alias: {
+        vue$: "vue/dist/vue.esm.js",
+        // Allows for better profiling with ReactDevTools
+        // ...(isEnvProductionProfile && {
+        //   'react-dom$': 'react-dom/profiling',
+        //   'scheduler/tracing': 'scheduler/tracing-profiling',
+        // }),
         ...(modules.webpackAliases || {}),
-      }
+      },
     },
     module: {
+      strictExportPresence: true,
       rules: [
         {
           test: /\.vue$/,
           loader: 'vue-loader',
         },
         {
-          test: /\.(js|jsx)$/,
+          test: /\.js$/,
           exclude: /node_modules/,
           use: {
               loader: 'babel-loader'
           }
-        }
+        },
       ]
     },
     plugins: [
+      new VueLoaderPlugin(),
+      // Generates an `index.html` file with the <script> injected.
       new HtmlWebpackPlugin(
         Object.assign(
           {},
@@ -101,15 +160,15 @@ module.exports = function(webpackEnv) {
           isEnvProduction ? {
               minify: {
                 removeComments: true,
-                // collapseWhitespace: true,
-                // removeRedundantAttributes: true,
-                // useShortDoctype: true,
-                // removeEmptyAttributes: true,
-                // removeStyleLinkTypeAttributes: true,
-                // keepClosingSlash: true,
-                // minifyJS: true,
-                // minifyCSS: true,
-                // minifyURLs: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true,
               }
             }
           : undefined
@@ -121,8 +180,40 @@ module.exports = function(webpackEnv) {
       // during a production build.
       // Otherwise React will be compiled in the very slow development mode.
       new webpack.DefinePlugin(env.stringified),
-    ],
-    stats: "verbose",
+      // This is necessary to emit hot updates (currently CSS only):
+      isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
+      // Generate an asset manifest file with the following content:
+      // - "files" key: Mapping of all asset filenames to their corresponding
+      //   output file so that tools can pick it up without having to parse
+      //   `index.html`
+      // - "entrypoints" key: Array of files which are included in `index.html`,
+      //   can be used to reconstruct the HTML if necessary
+      // new ManifestPlugin({
+      //   fileName: 'asset-manifest.json',
+      //   publicPath: publicPath,
+      //   generate: (seed, files, entrypoints) => {
+      //     const manifestFiles = files.reduce((manifest, file) => {
+      //       manifest[file.name] = file.path;
+      //       return manifest;
+      //     }, seed);
+      //     const entrypointFiles = entrypoints.main.filter(
+      //       fileName => !fileName.endsWith('.map')
+      //     );
+
+      //     return {
+      //       files: manifestFiles,
+      //       entrypoints: entrypointFiles,
+      //     };
+      //   },
+      // }),
+      // Moment.js is an extremely popular library that bundles large locale files
+      // by default due to how Webpack interprets its code. This is a practical
+      // solution that requires the user to opt into importing specific locales.
+      // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+      // You can remove this if you don't use Moment.js:
+      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    ].filter(Boolean),
+    stats: 'verbose',
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
     node: {
