f6b7a995bfb2e60320751aa275f9416fd03c24d5
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-04-13 03:37:13 -0500

    Edits structure of rules
     - Handles assets better by using 'oneOf' rule structure.
     - vue files are treated specially.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -244,148 +244,152 @@ module.exports = function(webpackEnv) {
       strictExportPresence: true,
       rules: [
         // { eslint }
-        // "url" loader works like "file" loader except that it embeds assets
-        // smaller than specified limit in bytes as data URLs to avoid requests.
-        // A missing `test` is equivalent to a match.
-        {
-          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-          loader: require.resolve('url-loader'),
-          options: {
-            limit: imageInlineSizeLimit,
-            name: 'static/media/[name].[hash:8].[ext]',
-          },
-        },
         // Process application Vue files.
         {
           test: /\.vue$/,
           loader: 'vue-loader',
         },
-        // Process application JS with Babel.
         {
-          test: /\.js$/,
-          include: paths.appSrc,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              // This is a feature of `babel-loader` for webpack (not Babel itself).
-              // It enables caching results in ./node_modules/.cache/babel-loader/
-              // directory for faster rebuilds.
-              cacheDirectory: true,
-              // TODO update to babel 7, there might be a mismatch
-              // GZip compression has barely any benefits, for either modes.
-              // https://github.com/facebook/create-react-app/issues/6846
-              // cacheCompression: false,
-              // compact: isEnvProduction,
-            },
-          },
-        },
-        // "postcss" loader applies autoprefixer to our CSS.
-        // "css" loader resolves paths in CSS and adds assets as dependencies.
-        // "vue-style" loader turns CSS and `<style>` blocks into JS modules that
-        // inject <style> tags into the index.html.
-        // In production, we use MiniCSSExtractPlugin to extract that CSS to a
-        // file, but in development, "style" loader enables hot editing of CSS.
-        {
-          test: /\.css$/,
           oneOf: [
+            // "url" loader works like "file" loader except that it embeds assets
+            // smaller than specified limit in bytes as data URLs to avoid requests.
+            // A missing `test` is equivalent to a match.
             {
-              // matches `<style module>` blocks
-              resourceQuery: /module/,
-              use: [
-                isEnvDevelopment && {
-                  loader: 'vue-style-loader',
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              loader: require.resolve('url-loader'),
+              options: {
+                limit: imageInlineSizeLimit,
+                name: 'static/media/[name].[hash:8].[ext]',
+              },
+            },
+            // Process application JS with Babel.
+            {
+              test: /\.js$/,
+              include: paths.appSrc,
+              use: {
+                loader: 'babel-loader',
+                options: {
+                  // This is a feature of `babel-loader` for webpack (not Babel itself).
+                  // It enables caching results in ./node_modules/.cache/babel-loader/
+                  // directory for faster rebuilds.
+                  cacheDirectory: true,
+                  // TODO update to babel 7, there might be a mismatch
+                  // GZip compression has barely any benefits, for either modes.
+                  // https://github.com/facebook/create-react-app/issues/6846
+                  // cacheCompression: false,
+                  // compact: isEnvProduction,
                 },
-                isEnvProduction && {
-                  loader: MiniCssExtractPlugin.loader,
-                  options: shouldUseRelativeAssetPaths
-                    ? { publicPath: '../../' }
-                    : {},
-                },
+              },
+            },
+            // "postcss" loader applies autoprefixer to our CSS.
+            // "css" loader resolves paths in CSS and adds assets as dependencies.
+            // "vue-style" loader turns CSS and `<style>` blocks into JS modules that
+            // inject <style> tags into the index.html.
+            // In production, we use MiniCSSExtractPlugin to extract that CSS to a
+            // file, but in development, "style" loader enables hot editing of CSS.
+            {
+              test: /\.css$/,
+              oneOf: [
                 {
-                  loader: 'css-loader',
-                  options: {
-                    importLoaders: 1,
-                    modules: {
-                      localIdentName: isEnvProduction
-                        ? '[hash:base64]'
-                        : '[path][name]__[local]',
+                  // matches `<style module>` blocks
+                  resourceQuery: /module/,
+                  use: [
+                    isEnvDevelopment && {
+                      loader: 'vue-style-loader',
                     },
-                    sourceMap: isEnvProduction && shouldUseSourceMap,
-                  },
-                },
-                // {
-                //   loader: 'postcss-loader',
-                //   options: {
-                //     ident: 'postcss',
-                //     plugins: () => [
-                //       require('postcss-flexbugs-fixes'),
-                //       require('postcss-preset-env')({
-                //         autoprefixer: {
-                //           flexbox: 'no-2009',
-                //         },
-                //         stage: 3,
-                //       }),
-                //     ],
-                //     sourceMap: isEnvProduction && shouldUseSourceMap,
-                //   },
-                // },
-              ].filter(Boolean),
-            },
-            {
-              // matches CSS files and plain `<style>` or `<style scoped>` blocks
-              use: [
-                isEnvDevelopment && {
-                  loader: 'vue-style-loader',
-                },
-                isEnvProduction && {
-                  loader: MiniCssExtractPlugin.loader,
-                  options: shouldUseRelativeAssetPaths
-                    ? { publicPath: '../../' }
-                    : {},
+                    isEnvProduction && {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: shouldUseRelativeAssetPaths
+                        ? { publicPath: '../../' }
+                        : {},
+                    },
+                    {
+                      loader: 'css-loader',
+                      options: {
+                        importLoaders: 1,
+                        modules: {
+                          localIdentName: isEnvProduction
+                            ? '[hash:base64]'
+                            : '[path][name]__[local]',
+                        },
+                        sourceMap: isEnvProduction && shouldUseSourceMap,
+                      },
+                    },
+                    {
+                      loader: 'postcss-loader',
+                      options: {
+                        ident: 'postcss',
+                        plugins: () => [
+                          require('postcss-flexbugs-fixes'),
+                          require('postcss-preset-env')({
+                            autoprefixer: {
+                              flexbox: 'no-2009',
+                            },
+                            stage: 3,
+                          }),
+                        ],
+                        sourceMap: isEnvProduction && shouldUseSourceMap,
+                      },
+                    },
+                  ].filter(Boolean),
                 },
                 {
-                  loader: 'css-loader',
-                  options: {
-                    importLoaders: 1,
-                    sourceMap: isEnvProduction && shouldUseSourceMap,
-                  },
+                  // matches CSS files and plain `<style>` or `<style scoped>` blocks
+                  use: [
+                    isEnvDevelopment && {
+                      loader: 'vue-style-loader',
+                    },
+                    isEnvProduction && {
+                      loader: MiniCssExtractPlugin.loader,
+                      options: shouldUseRelativeAssetPaths
+                        ? { publicPath: '../../' }
+                        : {},
+                    },
+                    {
+                      loader: 'css-loader',
+                      options: {
+                        importLoaders: 1,
+                        sourceMap: isEnvProduction && shouldUseSourceMap,
+                      },
+                    },
+                    {
+                      loader: 'postcss-loader',
+                      options: {
+                        ident: 'postcss',
+                        plugins: () => [
+                          require('postcss-flexbugs-fixes'),
+                          require('postcss-preset-env')({
+                            autoprefixer: {
+                              flexbox: 'no-2009',
+                            },
+                            stage: 3,
+                          }),
+                        ],
+                        sourceMap: isEnvProduction && shouldUseSourceMap,
+                      },
+                    },
+                  ].filter(Boolean),
                 },
-                // {
-                //   loader: 'postcss-loader',
-                //   options: {
-                //     ident: 'postcss',
-                //     plugins: () => [
-                //       require('postcss-flexbugs-fixes'),
-                //       require('postcss-preset-env')({
-                //         autoprefixer: {
-                //           flexbox: 'no-2009',
-                //         },
-                //         stage: 3,
-                //       }),
-                //     ],
-                //     sourceMap: isEnvProduction && shouldUseSourceMap,
-                //   },
-                // },
-              ].filter(Boolean),
+              ],
             },
-          ],
-        },
-        // "file" loader makes sure those assets get served by WebpackDevServer.
-        // When you `import` an asset, you get its (virtual) filename.
-        // In production, they would get copied to the `build` folder.
-        // This loader doesn't use a "test" so it will catch all modules
-        // that fall through the other loaders.
-        {
-          loader: require.resolve('file-loader'),
-          // Exclude `js` files to keep "css" loader working as it injects
-          // its runtime that would otherwise be processed through "file" loader.
-          // Also exclude `html` and `json` extensions so they get processed
-          // by webpacks internal loaders.
-          exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
-          options: {
-            name: 'static/media/[name].[hash:8].[ext]',
-          },
-        },
+            // "file" loader makes sure those assets get served by WebpackDevServer.
+            // When you `import` an asset, you get its (virtual) filename.
+            // In production, they would get copied to the `build` folder.
+            // This loader doesn't use a "test" so it will catch all modules
+            // that fall through the other loaders.
+            {
+              loader: require.resolve('file-loader'),
+              // Exclude `js` files to keep "css" loader working as it injects
+              // its runtime that would otherwise be processed through "file" loader.
+              // Also exclude `html` and `json` extensions so they get processed
+              // by webpacks internal loaders.
+              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              options: {
+                name: 'static/media/[name].[hash:8].[ext]',
+              },
+            },
+          ]
+        }
       ],
     },
     plugins: [
