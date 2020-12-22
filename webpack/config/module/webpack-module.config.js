a85fcc228e7839e201780678da25f5377a80e6a9
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-12-22 17:42:31 -0600

    Moves loader configs logic
     - Helps make webpack compilation a little faster by having to load
        less modules.

--- webpack/config/module/webpack-module.config.js
+++ webpack/config/module/webpack-module.config.js
@@ -1,4 +1,107 @@
-const loaders = require('./_loaders');
+const stylesUtil = require('./_styles-util');
+const paths = require('../../utils/paths');
+
+const configureEslintLoader = () => ({
+  test: /\.(js|mjs|vue)$/,
+  enforce: 'pre',
+  use: [
+    {
+      options: {
+        cache: true,
+        eslintPath: require.resolve('eslint'),
+        resolvePluginsRelativeTo: __dirname,
+      },
+      loader: require.resolve('eslint-loader'),
+    },
+  ],
+  include: paths.appSrc,
+});
+
+// Process application JS with Babel.
+const configureBabelLoader = () => ({
+  test: /\.(js|mjs)$/,
+  include: paths.appSrc,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      // This is a feature of `babel-loader` for webpack (not Babel itself).
+      // It enables caching results in ./node_modules/.cache/babel-loader/
+      // directory for faster rebuilds.
+      cacheDirectory: true,
+      // TODO update to babel 7, there might be a mismatch
+      // GZip compression has barely any benefits, for either modes.
+      // https://github.com/facebook/create-react-app/issues/6846
+      // cacheCompression: false,
+      // compact: isEnvProduction,
+    },
+  },
+});
+
+// Process application Vue files.
+const configureVueLoader = () => ({
+  test: /\.vue$/,
+  loader: 'vue-loader',
+});
+
+const configureStyleLoader = (args) => {
+  if (!(args.type in stylesUtil.styleTypes)) {
+    throw new TypeError(`No style loader configuration for type ${args.type}`, args);
+  }
+
+  const styleLoaderOptions = stylesUtil.styleTypes[args.type];
+  return {
+    ...styleLoaderOptions,
+    use: stylesUtil.getStyleLoaders(args),
+  };
+};
+
+// "file" loader makes sure those assets get served by WebpackDevServer.
+// When you `import` and asset. you get its (virtual) filename.
+// In production, they would get copied to the `build` folder.
+// This loader doesn't use a "test" so it will catch all modules
+// That fall through the other
+const configureFallbackLoader = () => ({
+  loader: require.resolve('file-loader'),
+  // Exclude `js` files to keep "css" loader working as it injects
+  // its runtime that would otherwise be processed through "file" loader.
+  // Also exclude `html` and `json` extensions so they get processed
+  // by webpack's internal
+  exclude: [/\.(js|mjs|vue)$/, /\.html$/, /\.json$/],
+  options: {
+    name: 'static/media/[name].[hash:8].[ext]',
+  },
+});
+
+// "url" loader works like "file" loader except that it embeds assets
+// smaller than specified limit in bytes as data URLs to avoid requests.
+const configureImageLoader = (imageInlineSizeLimit) => ({
+  test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.(svg)(\?.*)?$/],
+  loader: require.resolve('url-loader'),
+  options: {
+    limit: imageInlineSizeLimit,
+    name: 'static/img/[name].[hash:8].[ext]',
+  },
+});
+// "url" loader works like "file" loader except that it embeds assets
+// smaller than specified limit in bytes as data URLs to avoid requests.
+const configureMediaLoader = (imageInlineSizeLimit) => ({
+  test: [/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/],
+  loader: require.resolve('url-loader'),
+  options: {
+    limit: imageInlineSizeLimit,
+    name: 'static/media/[name].[hash:8].[ext]',
+  },
+});
+// "url" loader works like "file" loader except that it embeds assets
+// smaller than specified limit in bytes as data URLs to avoid requests.
+const configureFontLoader = (imageInlineSizeLimit) => ({
+  test: [/\.(woff2?|eot|ttf|otf)(\?.*)?$/i],
+  loader: require.resolve('url-loader'),
+  options: {
+    limit: imageInlineSizeLimit,
+    name: 'static/fonts/[name].[hash:8].[ext]',
+  },
+});
 
 module.exports = (envState) => {
   const { isEnvProduction, shouldUseSourceMap, imageInlineSizeLimit } = envState;
@@ -8,23 +111,23 @@ module.exports = (envState) => {
     rules: [
       // First, run the linter.
       // It's important to do this before Babel processes the JS.
-      loaders.configureEslintLoader(),
-      loaders.configureVueLoader(),
+      configureEslintLoader(),
+      configureVueLoader(),
       {
         // "oneOf" will traverse all of the following loaders until one will match
         // the requirements. When no loader matches, it will fall back to the
         // "file" loader at the end of the loader list.
         oneOf: [
-          loaders.configureImageLoader(imageInlineSizeLimit),
-          loaders.configureMediaLoader(imageInlineSizeLimit),
-          loaders.configureFontLoader(imageInlineSizeLimit),
-          loaders.configureBabelLoader(),
+          configureImageLoader(imageInlineSizeLimit),
+          configureMediaLoader(imageInlineSizeLimit),
+          configureFontLoader(imageInlineSizeLimit),
+          configureBabelLoader(),
           {
             test: /\.css$/,
             oneOf: [
               {
                 resourceQuery: /module/,
-                ...loaders.configureStyleLoader({
+                ...configureStyleLoader({
                   type: 'cssModules',
                   cssLoaderOptions: {
                     importLoaders: 1,
@@ -39,7 +142,7 @@ module.exports = (envState) => {
                   ...envState,
                 }),
               },
-              loaders.configureStyleLoader({
+              configureStyleLoader({
                 type: 'css',
                 cssLoaderOptions: {
                   importLoaders: 1,
@@ -54,7 +157,7 @@ module.exports = (envState) => {
             oneOf: [
               {
                 resourceQuery: /module/,
-                ...loaders.configureStyleLoader({
+                ...configureStyleLoader({
                   type: 'scssModules',
                   cssLoaderOptions: {
                     importLoaders: 3,
@@ -78,7 +181,7 @@ module.exports = (envState) => {
                   ...envState,
                 }),
               },
-              loaders.configureStyleLoader({
+              configureStyleLoader({
                 type: 'scss',
                 cssLoaderOptions: {
                   importLoaders: 3,
@@ -97,7 +200,7 @@ module.exports = (envState) => {
               }),
             ],
           },
-          loaders.configureFallbackLoader(),
+          configureFallbackLoader(),
         ],
       },
     ],
