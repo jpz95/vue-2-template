3902c3a629a0737986d6dcd0c75b678830d21325
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-12-22 18:08:19 -0600

    Adds router and store modules
     - Provides a starting point for app routing and state management.
     - Project can uncomment the needed modules, according to their needs.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import Vue from 'vue';
 // Import global styles
 import 'styles/';
 
+// import router from 'routes';
+// import store from 'store';
+
 import Root from './root.vue';
 
 new Vue({
   render: (h) => h(Root),
+  // router,
+  // store,
 }).$mount('#root');
