3bce2aad527a83d87d05a8eca94c6773021a31f2
J.P. Zambrano <jpzambrano95@hotmail.com>
2020-12-22 20:45:35 -0600

    Adds react-dev-utils and manifest plugin
     - Adds helpful webpack plugins from create-react-app.
     - Generates asset manifest.

--- webpack/config/webpack-plugins.config.js
+++ webpack/config/webpack-plugins.config.js
@@ -1,8 +1,13 @@
 const webpack = require('webpack');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
+const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const WatchMissingNodeModulesPlugin = require('react-dev-utils/WatchMissingNodeModulesPlugin');
 
 const paths = require('../utils/paths');
 const getEnvironmentVariables = require('../utils/load-env');
@@ -42,6 +47,23 @@ module.exports = (envState) => {
       },
     ),
 
+    // Inlines the webpack runtime script. This script is too small to warrant
+    // a network request.
+    // https://github.com/facebook/create-react-app/issues/5358
+    isEnvProduction
+    && new InlineChunkHtmlPlugin(HtmlWebpackPlugin, [/runtime-.+[.]js/]),
+
+    // Makes some environment variables available in index.html.
+    // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
+    // <link rel="icon" href="%PUBLIC_URL%/favicon.ico">
+    // It will be an empty string unless you specify "homepage"
+    // in `package.json`, in which case it will be the pathname of that URL.
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, envVariables.raw),
+
+    // This gives some necessary context to module not found errors, such as
+    // the requesting resource.
+    new ModuleNotFoundPlugin(paths.appPath),
+
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'production') { ... }. See `./load-env.js`.
     // It is absolutely essential that NODE_ENV is set to production
@@ -53,6 +75,13 @@ module.exports = (envState) => {
     isEnvDevelopment && new webpack.HotModuleReplacementPlugin(),
 
     !isEnvTest && new FriendlyErrorsWebpackPlugin(),
+
+    // If you require a missing module and then `npm install` it, you still have
+    // to restart the development server for webpack to discover it. This plugin
+    // makes the discovery automatic so you don't have to restart.
+    // See https://github.com/facebook/create-react-app/issues/186
+    isEnvDevelopment
+    && new WatchMissingNodeModulesPlugin(paths.appNodeModules),
 
     isEnvProduction
     && new MiniCssExtractPlugin({
@@ -68,25 +97,25 @@ module.exports = (envState) => {
     //   `index.html`
     // - "entrypoints" key: Array of files which are included in `index.html`,
     //   can be used to reconstruct the HTML if necessary
-    // TODO uncomment if we can use plugin
-    // new ManifestPlugin({
-    //   fileName: 'asset-manifest.json',
-    //   publicPath: publicPath,
-    //   generate: (seed, files, entrypoints) => {
-    //     const manifestFiles = files.reduce((manifest, file) => {
-    //       manifest[file.name] = file.path;
-    //       return manifest;
-    //     }, seed);
-    //     const entrypointFiles = entrypoints.main.filter(
-    //       fileName => !fileName.endsWith('.map')
-    //     );
+    new ManifestPlugin({
+      fileName: 'asset-manifest.json',
+      publicPath: paths.publicUrlOrPath,
+      generate: (seed, files, entrypoints) => {
+        const manifestFiles = files.reduce((manifest, file) => {
+          // eslint-disable-next-line no-param-reassign
+          manifest[file.name] = file.path;
+          return manifest;
+        }, seed);
+        const entrypointFiles = entrypoints.main.filter(
+          (fileName) => !fileName.endsWith('.map'),
+        );
 
-    //     return {
-    //       files: manifestFiles,
-    //       entrypoints: entrypointFiles,
-    //     };
-    //   },
-    // }),
+        return {
+          files: manifestFiles,
+          entrypoints: entrypointFiles,
+        };
+      },
+    }),
 
     // Moment.js is an extremely popular library that bundles large locale files
     // by default due to how Webpack interprets its code. This is a practical
